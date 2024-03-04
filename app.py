a37c9ac199e0a05d37f80b7de8db9ec5ab228325
Saul Chirinos <saul.chirinos10@gmail.com>
2024-03-03 18:07:36 -0700

    Batch update

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 import streamlit as st
-from optimal_wealth_fraction import merton_share
+import plotly.graph_objs as go
 
+import optimal_wealth_fraction as owf
+
+st.set_page_config(page_title=None, page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None)
+
+
+# Initialize session state variables if they don't exist
+if 'initialized' not in st.session_state:
+    st.session_state['initialized'] = True
+    st.session_state['wealth'] = 1000  # Default
+    st.session_state['risk_aversion'] = 2  # Default
+    st.session_state['long_term_weight'] = 0.5  # Default
 
 
 st.title('Optimal Wealth Fraction Calculator')
 
-# How to use
 with st.expander('READ ME'):
     st.markdown("""
-            ### Background
+            ##### Background
             
             > :heavy_exclamation_mark: Currently, the app only supports the S&P500 ETF (ticker: SPY). 
             
@@ -19,14 +29,17 @@ with st.expander('READ ME'):
             **Expected wealth** is the average financial outcome, focusing on increasing financial assets without considering how they impact happiness or satisfaction. For example, the happiness boost from increasing wealth from \$10,000 to \$100,000 is typically greater than from \$1 million to \$2 million. 
             **Expected utility** focuses on maximizing happiness from financial decisions, considering personal preferences, risk tolerance, and diminishing returns from wealth. Diminishing marginal returns refers to getting less utility the more wealth you acquire. The joy from gaining the first \$1,000 is often greater than from the subsequent \$1,000s.
 
-            ### How it works
+            ##### How it works
             
-            Input your amount of wealth available to invest and your risk aversion score.
+            1. Input your amount of wealth available to invest.
+            2. Input your risk aversion score.
             
-            Risk aversion score:
-            - 1 = Risk lover
-            - 2 = Balanced
-            - 3 = Risk averse
+                Risk aversion score:
+                - 1 = Risk taker
+                - 2 = Balanced
+                - 3 = Risk averse
+            
+            2. Select how much weight to apply for long term market risk (past 5 years of data variability). The weight for long term market risk and the weight for short term market risk sum up to 1. Meaning that if the long-term risk weight is set to 0.6, then the short-term risk weight is automatically set to 0.4.
             
             If experiencing any issues, rerun the application. If issues persist, [contact me](saul.chirinos10@gmail.com) with a screenshot of the error.
             
@@ -34,13 +47,15 @@ with st.expander('READ ME'):
             
             ---
             
-            **Data Sources**
-            - S&P500: [Online Data Robert Shiller](http://www.econ.yale.edu/~shiller/data.htm)
-            - Risk free rate: [FRED Treasury Inflation-Indexed Note](https://fred.stlouisfed.org/series/DTP10J28)
+            ##### Data
+                       
+            - S&P500 Prices: [Rapid API](https://rapidapi.com/letscrape-6bRBa3QguO5/api/real-time-finance-data)
+            - S&P500 PE Ratio: [Longtermtrends](https://www.longtermtrends.net/sp500-price-earnings-shiller-pe-ratio/)
+            - Risk Free Rate: [FRED Treasury Inflation-Indexed Note](https://fred.stlouisfed.org/series/DTP10J28)
             
             ---
             
-            **Merton Share Formula**
+            ##### Merton Share Formula
             
             $\hat k = \\frac{\mu - r}{\gamma \sigma^2}$
             
@@ -53,45 +68,125 @@ with st.expander('READ ME'):
             """)
 
 
-st.header('S&P500')
 
-column1, column2 = st.columns(2)
-with column1:
-    wealth = st.text_input('Investable Wealth')
+container = st.container()
+
+column01, _ = container.columns(2)
+with column01:
+    column11, column21, column31 = st.columns(3)
     
-with column2:
-    risk_aversion = st.slider('Risk Aversion', 1, 3)
-
-submit_button = st.button('Submit')
-
-real_yield, risk_free_rate, market_risk, optimal_betting_fraction = merton_share(risk_aversion)
-
-if submit_button:
-    try:
-        wealth = float(wealth)
+    with column11:
+        wealth = st.number_input('Investable Wealth', min_value=0, step=100)
     
-    except TypeError as e:
-        st.error('Please enter a valid number.')
-        st.stop()
+    with column21:
+        risk_aversion = st.number_input('Risk Aversion', 1, 3, value=2, step=1)
+        st.session_state['risk_aversion'] = risk_aversion
+    
+    with column31:
+        long_term_weight = st.slider('Long Term Volatility Weight', min_value=0.25, max_value=1.0, value=st.session_state['long_term_weight'], step=0.05)
+        st.session_state['longterm_weight'] = long_term_weight
+ 
+
+
+st.session_state['df'] = owf.collect_data('SPY')
+session_df = st.session_state.get('df')
+
+session_df = owf.prelim_calculations(session_df)
+session_df = owf.merton_share(session_df, risk_aversion)
+
+st.session_state['df'] = owf.update_risk(session_df, risk_aversion, long_term_weight)
+session_df = st.session_state.get('df')
+
+latest_real_yield = session_df['Real_Yield'].iloc[-1]
+latest_risk_free_rate = session_df['TIPS_10yr'].iloc[-1]
+latest_cape = session_df['CAPE'].iloc[-1]
+latest_market_risk = session_df['Market_Risk'].iloc[-1]
+latest_obf = session_df['Optimal_Bet_Fraction'].iloc[-1] / 100
+
+st.session_state['wealth'] = wealth
+
+    
+# Display key metrics        
+with container:
+    st.header('S&P 500')
+    
+    column21, column22, column32, column42, column52, column62 = st.columns(6)
+    with column21:
+        st.metric('Real Yield (1/CAPE)', f'{latest_real_yield*100:.1f}%')
+    
+    with column22:
+        st.metric('Market Risk', f'{latest_market_risk*100:.1f}%')
+
+    with column32:
+        st.metric('Risk Free Rate', f'{latest_risk_free_rate*100:.1f}%')
+
+    with column42:
+        st.metric('CAPE', f'{latest_cape:.2f}')
+
+    with column52:
+        st.metric('Optimal Betting Fraction', f'{latest_obf*100:.1f}%')
+
+    with column62:
+        st.metric('Wealth to Invest', f'${wealth*latest_obf:,.00f}')
         
-    except ValueError as e:
-        st.error('Please enter a number.')
-        st.stop()
 
-    # column_01, column_02, column_03, column_04 = st.columns(4)
-    column_01, column_02 = st.columns(2)
+# Plots
+column03, column04 = st.columns(2)
+with column03:
+    with st.expander('**Historical Optimal Bet Fraction**'):
+        # Plot for optimal bet fraction
+        _ = session_df[session_df['Optimal_Bet_Fraction'].notnull()]
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=_.index, y=_['Optimal_Bet_Fraction'], name="Optimal Bet Fraction"))
+        fig.update_layout(title_text="", xaxis_title="Date", yaxis_title="Percent (%)")
+        fig.update_yaxes(tickprefix="", ticksuffix="%", showgrid=True)
+        st.plotly_chart(fig)
 
-    with column_01:
-        st.metric('Real Yield (1/CAPE)', f'{round(real_yield*100, 2)}%')
-        st.metric('Risk Free Rate (10yr TIPS)', f'{round(risk_free_rate*100, 2)}%')
-        st.metric('Optimal Betting Fraction', f'{round(optimal_betting_fraction, 2)}%')
+    with st.expander('**Historical Price**'):
+        # Plot for close price
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=session_df.index, y=session_df['Close'], name="Close Price"))
+        fig.update_layout(title_text="", xaxis_title="Date", yaxis_title="Price")
+        fig.update_yaxes(tickprefix="$", ticksuffix="", showgrid=True)
+        st.plotly_chart(fig)
 
-    with column_02:
-        st.metric('CAPE', f'{round(real_yield**-1, 2)}')
-        st.metric('Market Risk', f'{round(market_risk*100, 2)}%')
-        st.metric('Wealth to Invest', f'${wealth*optimal_betting_fraction/100:.02f}')
+    with st.expander('**Historical CAPE (Shiller PE 10 Ratio)**'):
+        # Plot for CAPE
+        _ = session_df[session_df['CAPE'].notnull()]
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=_.index, y=_['CAPE'], name="CAPE"))
+        fig.update_layout(title_text="", xaxis_title="Date", yaxis_title="S&P500 PE 10")
+        st.plotly_chart(fig)
 
-    # with column_03:
 
-    # with column_04:
+with column04:
+    with st.expander('**Historical Market Risk**'):
+        # Plot for market volatility                   
+        _ = session_df[session_df['Market_Risk'].notnull()]
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=_.index, y=_['Market_Risk']*100, name="Market Risk"))
+        fig.update_layout(title_text="", xaxis_title="Date", yaxis_title="Percent (%)")
+        fig.update_yaxes(tickprefix="", ticksuffix="%", showgrid=True)
+        st.plotly_chart(fig)
+
+    with st.expander('**Historical Real Yield and 10-Yr Treasury**'):
+        # Plot for TIPS_10yr and Real_Yield
+        _ = session_df[session_df['TIPS_10yr'].notnull()]
+        fig = go.Figure()
+
+        fig.add_trace(go.Scatter(x=_.index, y=_['TIPS_10yr']*100, name="TIPS 10yr", marker_color='Green'))
+        fig.add_trace(go.Scatter(x=_.index, y=_['Real_Yield']*100, name="S&P500 Real Yield", marker_color='RoyalBlue'))
+        fig.update_layout(
+            title_text="",
+            xaxis_title="Date",
+            yaxis_title="Percent %",
+            yaxis_range=[
+                min(_['TIPS_10yr'].min()*100, _['Real_Yield'].min()*100), 
+                max(_['TIPS_10yr'].max()*100, _['Real_Yield'].max()*100)
+            ]
+        )
+        fig.update_yaxes(tickprefix="", ticksuffix="%", showgrid=True)
+        st.plotly_chart(fig)
+
         
+            
--- optimal_wealth_fraction.py
+++ optimal_wealth_fraction.py
@@ -1,80 +1,216 @@
 import os
 import pandas as pd
+import polars as pl
+import numpy as np
 import requests
 import streamlit as st
 import xlrd
 
 
 FRED_API_KEY = os.getenv('FRED_API_KEY')
-# from config import FRED_API_KEY  # Remove when pushing to github
-
-
-def get_shiller_pe_ratio():
-    """Returns the current CAPE of the SP500.
-
-    Returns:
-        _type_: _description_
-    """
-    url = 'http://www.econ.yale.edu/~shiller/data/ie_data.xls'
-
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        with open('Data/shiller_data.xls', 'wb') as file:
-            file.write(response.content)
-            data = pd.read_excel('Data/shiller_data.xls', sheet_name='Data', skiprows=7, usecols='A, M')  # Date, CAPE
-            
-            data = data.dropna().reset_index(drop=True)
-            cape = data.iloc[-1, 1]  # Cyclically Adjusted PE Ratio aka Shiller PE Ratio
-            
-            return cape
-    else:
-        print("Failed to retrieve Shiller data document.")
-        return None
+RAPID_API_KEY = os.getenv('RAPID_API_KEY')
+# from config import FRED_API_KEY, RAPID_API_KEY
     
 
-def get_risk_free_rate():
-    # play around with param frequency (d default): d = Daily, w = Weekly, bw = Biweekly, m = Monthly, q = Quarterly, sa = Semiannual, a = Annual
-    url = f'https://api.stlouisfed.org/fred/series/observations?series_id=DTP10J28&api_key={FRED_API_KEY}&frequency=d&sort_order=desc&file_type=json'
+@st.cache_data
+def get_price_history(ticker:str):
+    url = "https://real-time-finance-data.p.rapidapi.com/stock-time-series"
+
+    querystring = {"symbol":ticker,"period":"MAX","language":"en"}
+
+    headers = {
+        "X-RapidAPI-Key": RAPID_API_KEY,
+        "X-RapidAPI-Host": "real-time-finance-data.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+    
+    try:
+        assert response.status_code == 200
+        
+        return response.json().get('data')['time_series']
+        
+    except AssertionError as e:
+        st.error(e)
+        st.stop()
+    
+
+
+@st.cache_data
+def get_cpi_data(date_max):
+    url = f'https://api.stlouisfed.org/fred/series/observations?series_id=CPILFESL&api_key={FRED_API_KEY}&file_type=json'
     response = requests.get(url)
     
     try:
         assert response.status_code == 200
     
-        risk_free_rate = float(response.json().get('observations')[0].get('value'))  # most recent 10 yr TIPS rate
-        risk_free_rate /= 100
+        data = pd.DataFrame(response.json().get('observations'))
+        data = data[['date', 'value']]
+                
+        data['date'] = pd.to_datetime(data.date)
+        data['date'] = data.date.apply(lambda date: date.date())
+        data['date'] = pd.to_datetime(data.date)
+        data['value'] = data.value.astype(float)
         
-        return risk_free_rate
-
-    except AssertionError:
-        return None
-
-
-def get_market_risk_volatility():
-    df = pd.read_excel('Data/shiller_data.xls', sheet_name='Data', skiprows=7)
-    
-    df = df.dropna(subset=['Date'])
-    
-    df['Monthly_Returns'] = df['Price'].pct_change()
+        # Get latest record
+        data = data.set_index('date')
         
-    # Short-Horizon Volatility (last 3 months)
-    short_horizon_volatility = df['Monthly_Returns'][-3:].std()
+        date_rng = pd.date_range(start=data.index.min().date(), end=date_max, freq='D')
+        data = data.reindex(date_rng)
+        data['value'] = data.value.fillna(method='ffill')
+        
+        data = data.reset_index()
+        data.columns = ['Date', 'CPI']
+        
+        return data
 
-    # Long-Horizon Volatility (last 5 years)
-    long_horizon_volatility = df['Monthly_Returns'][-60:].std()
+    except AssertionError as e:
+        st.error(e)
+        st.stop()
+    
 
-    # Blend the volatilities with equal weighting (50% each) -> play around with weight parameters
-    blended_volatility = (short_horizon_volatility + long_horizon_volatility) / 2
+@st.cache_data
+def get_10yr_tips(date_max):
+    # Param frequency (d default): d = Daily, w = Weekly, bw = Biweekly, m = Monthly, q = Quarterly, sa = Semiannual, a = Annual
+    url = f'https://api.stlouisfed.org/fred/series/observations?series_id=DTP10J28&api_key={FRED_API_KEY}&frequency=d&sort_order=desc&file_type=json'
+    response = requests.get(url)
+    
+    try:
+        assert response.status_code == 200
+        series = response.json().get('observations')
+        
+        dates = [record.get('date') for record in series]
+        risk_free_rate_series = [record.get('value') for record in series]
+        risk_free_rate_series = [float(value) if value != '.' else np.nan for value in risk_free_rate_series]
+        
+        data = pd.DataFrame({'Date': dates, 'TIPS_10yr': risk_free_rate_series})
 
-    return blended_volatility
+        data['Date'] = pd.to_datetime(data.Date)
+        data['Date'] = data.Date.apply(lambda date: date.date())
+        data['Date'] = pd.to_datetime(data.Date)
+        
+        # Get latest record
+        data = data.sort_values('Date')
+        data = data.set_index('Date')
+        date_rng = pd.date_range(start=data.index.min().date(), end=date_max, freq='D')
+        data = data.reindex(date_rng)
+        
+        data['TIPS_10yr'] = data['TIPS_10yr'].fillna(method='ffill') / 100
+    
+        data = data.reset_index()
+        data.columns = ['Date', 'TIPS_10yr']
+                
+        return data
+
+    except AssertionError as e:
+        st.error(e)
+        st.stop()
 
 
 @st.cache_data
-def merton_share(risk_aversion):
-    real_yield = 1 / get_shiller_pe_ratio()
-    risk_free_rate = get_risk_free_rate()
-    market_risk = get_market_risk_volatility()
+def get_price_data(ticker):
+    series_dict = get_price_history(ticker)
+    
+    dates = series_dict.keys()
+    prices = [hash.get('price') for hash in series_dict.values()]
 
-    optimal_betting_fraction = (real_yield - risk_free_rate) / (risk_aversion * market_risk**2)
+    df = pd.DataFrame({'Date': dates, 'Close': prices})
+    df['Date'] = pd.to_datetime(df.Date)
+    df['Date'] = df.Date.apply(lambda date: date.date())
+    df['Date'] = pd.to_datetime(df.Date)
 
-    return real_yield, risk_free_rate, market_risk, optimal_betting_fraction
+    df = df.drop_duplicates()
+    df = df.set_index('Date')
+
+    up_to_date = df.index.max().date()
+    date_rng = pd.date_range(start=df.index.min().date(), end=up_to_date, freq='D')
+    df = df.reindex(date_rng)
+
+    df_interpolated = df.interpolate(method='linear')
+
+    df_interpolated = df_interpolated.reset_index()
+    df_interpolated.columns = ['Date', 'Close']
+    
+    return df_interpolated, up_to_date
+
+
+@st.cache_data
+def get_pe_data(date_max):
+    pe_data = pl.read_csv('Data/sp-500-price-earnings-ra.csv')
+    pe_data = pe_data.with_columns(pl.col('DateTime').str.strptime(pl.Datetime, format="%Y-%m-%d %H:%M:%S"))
+    pe_data = pe_data.to_pandas()
+    pe_data.columns = ['Date', 'PE_Ratio']
+    
+    # Get latest record
+    pe_data = pe_data.set_index('Date')
+    
+    date_rng = pd.date_range(start=pe_data.index.min().date(), end=date_max, freq='D')
+    pe_data = pe_data.reindex(date_rng)
+    pe_data['PE_Ratio'] = pe_data['PE_Ratio'].fillna(method='ffill')
+    
+    pe_data = pe_data.reset_index()
+    pe_data.columns = ['Date', 'PE_Ratio']
+    
+    return pe_data
+
+
+def collect_data(ticker:str):
+    
+    price_df, up_to_date = get_price_data(ticker)
+    cpi_df = get_cpi_data(up_to_date)
+    pe_df = get_pe_data(up_to_date)
+    tips_10yr = get_10yr_tips(up_to_date)
+        
+    data = price_df.merge(cpi_df, on='Date', how='left') \
+        .merge(pe_df, on='Date', how='left') \
+        .merge(tips_10yr, on='Date', how='left')
+    
+    return data
+
+
+@st.cache_data
+def prelim_calculations(df):
+    copy_df = df.copy()
+    try:
+        copy_df['Date'] = pd.to_datetime(copy_df['Date'])    
+        copy_df = copy_df.set_index('Date')
+        
+        copy_df['Real_Price'] = copy_df.Close / copy_df.CPI * copy_df.CPI.iloc[-1]
+        copy_df['Earnings'] = copy_df.Close / copy_df.PE_Ratio
+        copy_df['Real_Earnings'] = copy_df.Earnings / copy_df.CPI * copy_df.CPI.iloc[-1]
+        copy_df['CAPE'] = copy_df.Real_Price / copy_df.Real_Earnings.rolling(window=30*12*10).mean()
+        copy_df['Real_Yield'] = 1 / copy_df.CAPE
+        
+        monthly_df = copy_df[['Close']].resample('M').mean()  # Resample to monthly, forward fill any missing data
+        copy_df['Monthly_Returns'] = monthly_df['Close'].pct_change()
+        copy_df['Monthly_Returns'] = copy_df['Monthly_Returns'].fillna(method='ffill')
+        
+        # Short and long term market volatility (last 3 months and 5 years)
+        copy_df['Market_Risk']= (copy_df.Monthly_Returns.rolling(window=90).std() + copy_df.Monthly_Returns.rolling(window=30*12*5).std()) / 2  
+    
+        return copy_df
+    
+    except TypeError as e:
+        st.error(e)
+        st.stop()
+
+
+@st.cache_data
+def merton_share(data, risk_aversion:int):
+    copy_df = data.copy()
+    copy_df['Optimal_Bet_Fraction'] = (copy_df.Real_Yield - copy_df.TIPS_10yr) / (risk_aversion * copy_df.Market_Risk**2)
+
+    return copy_df
+
+
+@st.cache_data
+def update_risk(df, risk_aversion, longterm_weight):
+    copy_df = df.copy()
+    
+    # Short and long term market volatility (last 3 months and 5 years)
+    copy_df['Market_Risk']= (1-longterm_weight)*copy_df.Monthly_Returns.rolling(window=90).std() + longterm_weight*copy_df.Monthly_Returns.rolling(window=30*12*5).std()
+
+    # Recalculate optimal bet fraction
+    copy_df = merton_share(copy_df, risk_aversion)
+    
+    return copy_df
